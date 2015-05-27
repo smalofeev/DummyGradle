67754b0d6132763e6d25571c199c39212f4700eb
smalofeev <smal@Pearl.softservecom.com>
2015-05-27 16:51:23 +0300

    first commit

---
+++ library/src/main/java/com/dummy/smal/dummygradle/DummyGradle.java
@@ -0,0 +1,12 @@
+package com.dummy.smal.dummygradle;
+
+/**
+ * Created by smal on 27.05.2015.
+ */
+public class DummyGradle {
+
+    @Override
+    public String toString() {
+        return "It's dummy gradle version 1";
+    }
+}
